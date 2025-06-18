ca3d496a0633ca19a94642101c08d189fa399704
bijuthottathil <bijuthottathil>
2025-06-18 22:06:45 +0000

    Notebook changes

--- script/unity_catalog_oil_sensor_setup.sql.sql
+++ script/unity_catalog_oil_sensor_setup.sql.sql
@@ -4,17 +4,22 @@
 
 -- COMMAND ----------
 
+
 -- Step 1: Create External Location (update <storage_account> and credential name)
-CREATE EXTERNAL LOCATION IF NOT EXISTS oil_ext_loc
-URL 'abfss://oil-lakehouse@<storage_account>.dfs.core.windows.net/medallion'
-WITH (STORAGE CREDENTIAL my_adls_credential);
+CREATE EXTERNAL LOCATION oil_ext_loc
+URL 'abfss://lakehouse@oillakehouse.dfs.core.windows.net/medallion'
+WITH (STORAGE CREDENTIAL azure_cred_devandtest);
 
 -- COMMAND ----------
 
 -- Step 2: Create Catalog and Schemas
-CREATE CATALOG IF NOT EXISTS oil_sensors_catalog;
-USE CATALOG oil_sensors_catalog;
+CREATE CATALOG IF NOT EXISTS oil_sensors_catalog MANAGED LOCATION 'abfss://lakehouse@oillakehouse.dfs.core.windows.net/medallion/catalog';
 
+-- COMMAND ----------
+
+-- Step 2: Create  Schemas
+
+USE CATALOG oil_sensors_catalog;
 CREATE SCHEMA IF NOT EXISTS bronze_schema;
 CREATE SCHEMA IF NOT EXISTS silver_schema;
 CREATE SCHEMA IF NOT EXISTS gold_schema;
@@ -25,14 +30,14 @@ CREATE SCHEMA IF NOT EXISTS gold_schema;
 -- Bronze
 CREATE TABLE IF NOT EXISTS bronze_schema.oil_sensor_data_raw
 USING DELTA
-LOCATION 'abfss://oil-lakehouse@<storage_account>.dfs.core.windows.net/medallion/bronze/oil_sensor_data';
+LOCATION 'abfss://lakehouse@oillakehouse.dfs.core.windows.net/medallion/bronze/oil_sensor_data';
 
 -- Silver
 CREATE TABLE IF NOT EXISTS silver_schema.oil_sensor_data_clean
 USING DELTA
-LOCATION 'abfss://oil-lakehouse@<storage_account>.dfs.core.windows.net/medallion/silver/oil_sensor_data_clean';
+LOCATION 'abfss://lakehouse@oillakehouse.dfs.core.windows.net/medallion/silver/oil_sensor_data_clean';
 
 -- Gold
 CREATE TABLE IF NOT EXISTS gold_schema.oil_sensor_summary
 USING DELTA
-LOCATION 'abfss://oil-lakehouse@<storage_account>.dfs.core.windows.net/medallion/gold/oil_sensor_summary';
+LOCATION 'abfss://lakehouse@oillakehouse.dfs.core.windows.net/medallion/gold/oil_sensor_summary';
